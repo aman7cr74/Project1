e353dc69fe3e1c9feba4f07ec5d6adc6fa8c0cd6
aman7cr7 <107622127+aman7cr7@users.noreply.github.com>
2026-01-12 20:39:00 +0530

    Added <what you changed>

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("Helllo aman from world war 1 , yoo ")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is my first Python web app tjhid aman from world war 2!"
+
+@app.route("/health")
+def health():
+    return {"status": "UP"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
